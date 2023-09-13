9fb43b98f33f991a796d8355850e46c2bbd33b26
rohithrjnr <140920798+rohithrjnr@users.noreply.github.com>
2023-09-13 21:08:00 +0530

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
+import { TiArrowSortedDown } from "react-icons/ti";
+import { TiArrowSortedUp } from "react-icons/ti";
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
@@ -22,18 +24,30 @@ const ExpenseItem = (props) => {
             type: 'ADD_EXPENSE',
             payload: expense
         });
-
     }
+    const DecreaseAllocation = (name) => {
+            const expense = {
+                name: name,
+                cost: 10,
+            };
+    
+            dispatch({
+                type: 'DECREASE_EXPENSE',
+                payload: expense
+            });
+    }
+
+
 
     return (
         <tr>
         <td>{props.name}</td>
         <td>${props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><TiArrowSortedUp onClick={event=> increaseAllocation(props.name)}></TiArrowSortedUp></td>
+        <td><TiArrowSortedDown onClick={event=> DecreaseAllocation(props.name)}></TiArrowSortedDown></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
 };
 
 export default ExpenseItem;
-
